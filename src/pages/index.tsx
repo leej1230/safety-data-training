fab440259c89d734a2a02baa82e7cbc8f641488e
jaewoo <leejaewoo1230@gmail.com>
2023-04-23 00:14:43 -0700

    nav bar is in module and added in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+    <Navbar />
       <p>
         Home Page
       </p>
