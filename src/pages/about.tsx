214d6b2d29a4dca73c2f08670c40f3f7356dd49b
neerjanatu <neerjanatu@gmail.com>
2023-04-24 16:11:06 -0700

    made a change to the about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
+import styles from '@/styles/about.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -12,8 +11,8 @@ export default function Home() {
         <title>About Page</title>
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <h1>About Page</h1>
+        <div className={styles.container}>
+          <h1>About</h1>
         </div>
       </main>
     </>
