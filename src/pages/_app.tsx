57e2128a600c6dc3ce58301f07272546c54b371b
leej1230 <leejaewoo1230@gmail.com>
2023-04-29 01:25:25 -0700

    finalized home page by setting up bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/Navbar.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
-import { Inter } from 'next/font/google'
-// import 'bootstrap'
+import Head from 'next/head'
 import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const Home = () => {
   return (
     <>
-    <Navbar />
-    <div className="position-relative">
-      <p className="position-absolute top-0 start-0 translate-middle">
-        Home Page
-      </p>
-    </div>
+      <Head>
+        <title>Safety Database</title>
+      </Head>
+
+      <Navbar />
+
+      <main className="d-flex flex-column justify-content-between align-items-center p-5 min-vh-100">
+        <div className="d-flex flex-column justify-content-between align-items-center p-5 min-vh-100">
+          <p className="d-flex justify-content-center align-items-center h-100 mt-5" style={{fontSize: '2.3rem'}}>
+            Home
+          </p>
+        </div>
+      </main>
+
     </>
   )
 }
+
+export default Home;
